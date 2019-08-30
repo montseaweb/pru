d760c781d2204505024169892f0198571318b1f1
Montse Viyuela <montsemsanz@gmail.com>
2019-08-30 11:40:06 +0200

    modificado main eliminada println

--- Main.java
+++ Main.java
@@ -7,16 +7,14 @@
  */
 public class Main
 {
-     
 
     /**
      *  
      */
     public static void main(String[] args){
         System.out.println("Hello world Hola mundo");
-		System.out.println("Probando");
-		int a = 10;
-		
-         
+
+        int a = 10;
+
     }
 }
