73e2a3caf8030f621a62b554478b31d659c2f9d7
montsemsanz <montsemsanz@gmail.com>
2019-08-30 11:34:49 +0200

    commit inicial

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class Main
+{
+     
+
+    /**
+     *  
+     */
+    public static void main(String[] args){
+        System.out.println("Hello world Hola mundo");
+		System.out.println("Probando");
+		int a = 10;
+		
+         
+    }
+}
